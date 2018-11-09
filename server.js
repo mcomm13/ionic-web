1517048352fc9fa801e0abe0ec64ba61af8331ad
Mike Commendatore <michael.commendatore@outlook.com>
2018-11-08 23:05:33 -0500

    Modify last commit

--- server.js
+++ server.js
@@ -9,22 +9,22 @@ const app = express();
 
 // Parsers
 app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: false
+//   })
+// );
 
 // Angular WWW output folder
-//app.use(express.static(path.join(__dirname, "www")));
+app.use(express.static(path.join(__dirname, "www")));
 
 // API location
 // app.use('/api', api);
 
 // Send all other requests to the Angular app
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "www/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "www/index.html"));
+});
 
 //Set Port
 const port = process.env.PORT || "8080";
