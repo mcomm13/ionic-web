caad2e50a9aa25ddeea70d0ed182afd28157e98a
Mike Commendatore <michael.commendatore@outlook.com>
2018-11-08 23:00:20 -0500

    Simplify server.js

--- server.js
+++ server.js
@@ -16,15 +16,15 @@ app.use(
 );
 
 // Angular WWW output folder
-app.use(express.static(path.join(__dirname, "www")));
+//app.use(express.static(path.join(__dirname, "www")));
 
 // API location
 // app.use('/api', api);
 
 // Send all other requests to the Angular app
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "www/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "www/index.html"));
+// });
 
 //Set Port
 const port = process.env.PORT || "8080";
