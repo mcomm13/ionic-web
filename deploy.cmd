7d8768ef3cd40e1b74ca38e1e0bcbee8e6b4b604
Mike Commendatore <michael.commendatore@outlook.com>
2018-11-09 23:06:04 -0500

    here goes

--- deploy.cmd
+++ deploy.cmd
@@ -103,16 +103,16 @@ IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
 IF EXIST "%DEPLOYMENT_SOURCE%/angular.json" (
 echo Building App in %DEPLOYMENT_SOURCE%…
 pushd "%DEPLOYMENT_SOURCE%"
-:: call :ExecuteCmd !NPM_CMD! run build
+call :ExecuteCmd !NPM_CMD! run build
 :: If the above command fails comment above and uncomment below one
-call :ExecuteCmd node_modules/.bin/ng build --prod
+::call :ExecuteCmd node_modules/.bin/ng build --prod
 IF !ERRORLEVEL! NEQ 0 goto error
 popd
 )
 
 :: 4. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/www" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd;node_modules"
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/www" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
