9c7c1a55d6dc0976527b9a351317fbbf4d73685e
Mike Commendatore <michael.commendatore@outlook.com>
2018-11-09 22:35:59 -0500

    deployment script change

--- deploy.cmd
+++ deploy.cmd
@@ -99,12 +99,15 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   popd
 )
 
-:: 3. Build application
-IF EXIST "%DEPLOYMENT_SOURCE%\angular.json" (
-  pushd "%DEPLOYMENT_SOURCE%"
-  call :ExecuteCmd node_modules\.bin\ng build --prod
-  IF !ERRORLEVEL! NEQ 0 goto error
-  popd
+:: 3. Angular Prod Build
+IF EXIST "%DEPLOYMENT_SOURCE%/angular.json" (
+echo Building App in %DEPLOYMENT_SOURCE%…
+pushd "%DEPLOYMENT_SOURCE%"
+call :ExecuteCmd !NPM_CMD! run build
+:: If the above command fails comment above and uncomment below one
+:: call ./node_modules/.bin/ng build –prod
+IF !ERRORLEVEL! NEQ 0 goto error
+popd
 )
 
 :: 4. KuduSync
