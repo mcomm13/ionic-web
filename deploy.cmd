f6e183a0b0f44af720ae6bee0cc229fe2e1c2c76
Mike Commendatore <michael.commendatore@outlook.com>
2018-11-09 22:45:56 -0500

    idk

--- deploy.cmd
+++ deploy.cmd
@@ -92,8 +92,8 @@ echo Handling node.js deployment for Ionic app
 call :SelectNodeVersion
 
 :: 2. Install npm packages
-IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
-  pushd "%DEPLOYMENT_TARGET%"
+IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
+  pushd "%DEPLOYMENT_SOURCE%"
   call :ExecuteCmd !NPM_CMD! install --production
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
