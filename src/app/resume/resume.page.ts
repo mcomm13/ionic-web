b1b96cf6fa866c71075a723359b1ecc152bff7de
Mike Commendatore <michael.commendatore@outlook.com>
2018-11-17 19:16:02 -0500

    Fix build issue

--- src/app/resume/resume.page.ts
+++ src/app/resume/resume.page.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import * as data from '../data/resume.json';
 
+interface Resume {
+  jobs: Array<object>;
+  skills: Array<string>;
+  education: Array<object>;
+  otherAccomplishments: Array<string>;
+}
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.page.html',
   styleUrls: ['./resume.page.scss']
 })
 export class ResumePage implements OnInit {
-  resumeData: object;
+  resumeData: Resume;
   constructor() {}
 
   ngOnInit() {
