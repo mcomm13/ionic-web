e6b3a1e62be5ea906edc4b06635529971cfcd9d2
Mike Commendatore <michael.commendatore@outlook.com>
2018-12-17 09:25:43 -0500

    idk

--- server.js
+++ server.js
@@ -8,11 +8,6 @@ const api = require('./server/routes/api');
 
 // Parsers
 app.use(bodyParser.json());
-// app.use(
-//   bodyParser.urlencoded({
-//     extended: false
-//   })
-// );
 
 // Angular WWW output folder
 app.use(express.static(path.join(__dirname, 'www')));
