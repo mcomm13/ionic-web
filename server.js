9d965a0c60d45aec47e146815f9ce58cefabf187
Mike Commendatore <michael.commendatore@outlook.com>
2018-12-17 12:57:52 -0500

    Trying a hack way

--- server.js
+++ server.js
@@ -2,9 +2,43 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
 
 // API file for interacting with MongoDB
-const api = require('./server/routes/api');
+// const api = require('./server/routes/api');
+
+// Connect to Mongo
+const url =
+  'mongodb+srv://mcomm:7qytNSjbwoTIqFdn@mcomm1-m715q.azure.mongodb.net/personal?retryWrites=true';
+
+const connection = closure => {
+  return MongoClient.connect(
+    url,
+    { useNewUrlParser: true },
+    (err, client) => {
+      if (err) {
+        return console.error(err);
+      }
+      console.log('Connected successfully to server');
+      const db = client.db();
+      closure(db);
+    }
+  );
+};
+
+// Response handling
+let response = {
+  status: 200,
+  data: [],
+  message: ''
+};
+
+// Error handling
+const sendError = (err, res) => {
+  response.status = 501;
+  response.message = typeof err === 'object' ? err.message : err;
+  res.send(501).json(response);
+};
 
 // Parsers
 app.use(bodyParser.json());
@@ -12,8 +46,39 @@ app.use(bodyParser.json());
 // Angular WWW output folder
 app.use(express.static(path.join(__dirname, 'www')));
 
-// API location
-app.use('/api', api);
+// APIs
+// app.use('/api', api);
+// Get blogs
+app.get('/api/blogs', (req, res) => {
+  connection(db => {
+    db.collection('blog')
+      .find()
+      .toArray()
+      .then(blogs => {
+        response.data = blogs;
+        res.json(response);
+      })
+      .catch(err => {
+        sendError(err, res);
+      });
+  });
+});
+
+// Get songs
+app.get('/api/music', (req, res) => {
+  connection(db => {
+    db.collection('music')
+      .find()
+      .toArray()
+      .then(music => {
+        response.data = music;
+        res.json(response);
+      })
+      .catch(err => {
+        sendError(err, res);
+      });
+  });
+});
 
 // Send all other requests to the Angular app
 app.get('*', (req, res) => {
